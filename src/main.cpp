087691068c2fb5626b981227bd0e96cba46566c2
Berndll <chwber@gmail.com>
2023-11-05 21:45:09 +0100

    Input first version

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,45 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+#include <fstream>
+#include <string>
 
 #include "Window.hpp"
 
 #define FPS 100 // default = 100
 
 int main() {
+    std::fstream input;
+    input.open("input/object.txt", std::ios::in);
+
+    if (input.is_open()) {
+        std::string str;
+
+        while (std::getline(input, str)) {
+            // for (int i = 0; i < str.size(); ++i) {
+            //     if (str.at(i) == ',') {
+            //         std::cout << "\n";
+            //     }
+            //     std::cout << str.at(i);
+            // }
+
+
+
+            // int it = 0, num;
+            // while (num != std::string::npos) {
+            //     num = static_cast<int>(temp.find(","));
+            //     for (int i = 0; i < num; ++i)
+            //         std::cout << temp.at(i) << "\n";
+            //         // std::cout << num << "\n";
+            //     it = 1;
+            // }
+        }
+        std::cout << "\n" << std::endl;
+        input.close();
+    }
+
+    return 0;
+
     Window window;
 
     // Matrix3D<double> threeD (3,3,3);
@@ -15,7 +48,6 @@ int main() {
     // return 0;
 
     Matrix2D<double> rotPoint(2,1);
-    // rotPoint = {100, 100};
     rotPoint.at(0,0) = 100;
     rotPoint.at(1,0) = 100;
     Matrix2D<double> rotMat(2,2);
@@ -35,9 +67,6 @@ int main() {
     points.at(2) = (160);
     points.at(3).at(0,0) = (160);
     points.at(3).at(1,0) = (40);
-        
-    for (auto p : points)
-        p.print();
 
     while (window.ProcessMessages()) {
         window.clearScreen(0x000000);
@@ -47,7 +76,7 @@ int main() {
         for (auto &p : points)
             p.rotate(rotPoint, deg);
 
-        if (deg > 2 * M_PI) 
+        if (deg >= 2 * M_PI) 
             deg -= 2 * M_PI;
 
         Sleep(1000 / FPS);
